6929587e1f2f3c46b1385920029863a7a8891dc7
Madson Santos <madsonsdj@hotmail.com>
2022-10-19 15:07:37 -0300

    tab transformado em calsse

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,27 +1,33 @@
 
-export default function initTabNav(){
-    const tabMenu = document.querySelectorAll('[data-tab="menu"] li')
-    const tabContent = document.querySelectorAll('[data-tab="content"] section')
-    tabContent[0].classList.add('ativo')
+export default class TabNav{
+    constructor(menu,content){
+        this.tabMenu = document.querySelectorAll(menu)
+        this.tabContent = document.querySelectorAll(content)
+        this.activeClass = 'ativo';
+    }
+ 
     
-    function activeTab(index){
-        tabContent.forEach((section)=>{
-            section.classList.remove('ativo')
+    activeTab(index){
+        this.tabContent.forEach((section)=>{
+            section.classList.remove(this.activeClass)
         })
         
-        const direcao = tabContent[index].dataset.anime;
-        tabContent[index].classList.add('ativo',direcao);
+        const direcao = this.tabContent[index].dataset.anime;
+        this.tabContent[index].classList.add(this.activeClass,direcao);
     }
 
-    if(tabMenu.length && tabContent.length){
+    addTabNavEvent(){
+        this.tabMenu.forEach((itemMenu, index)=>{
+            itemMenu.addEventListener('click', ()=> this.activeTab(index));
+        });       
+    }
 
- 
-
-        tabMenu.forEach((itemMenu, index)=>{
-            itemMenu.addEventListener('click', function(){
-                activeTab(index);
-            });
-        });
+    init(){
+        if(this.tabMenu.length && this.tabContent.length){
+        this.activeTab(0)
+        this.addTabNavEvent();
+    }
+        
     }
 }
 
